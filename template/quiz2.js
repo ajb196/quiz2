02fad9aa16786e93b60ba3763a4f7e0a94bfc879
ajb196 <austinbagnato@gmail.com>
2016-03-28 20:35:26 -0400

    Austin Bagnato Quiz 2 Submission

--- template/quiz2.js
+++ template/quiz2.js
@@ -4,34 +4,79 @@
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	var data = null;
+	var cur = null;
+
+	$(".titleBtn").click(function(){
+		var item = Math.floor(Math.random()*14);
+
+		if(data == null) {
+
+			$.ajax({
+				type:"GET",
+				url:"http://www.mattbowytz.com/simple_api.json",
+				dataType:"json",
+				data:"data=quizdata",
+				success:function(returned){
+					data = returned.data;
+					$("#title").html(data[item]);
+					cur = data[item];
+				}
+			});
+
+			$(this).html("Change It");
+			$(".keepBtn").fadeIn("fast");
+		} else {
+			$("#title").html(data[item]);
+			cur = data[item];
+		}
+		
+	});
+
+	$(".keepBtn").click(function(){
+		document.cookie = "title=" + cur + ";";
+	});
+
+	var $mouseover = $('.mouseover');
+	var $click     = $('.click');
+	var $sub       = $('.submit');
+	var $timeout   = $('.timeout');
+	var $submit    = $('.submit');
+
+	$timeout.hide(0);
+		$(".keepBtn").hide(0);
 
 	$mouseover.on('mouseover', function() {
-		$this = $(this);
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	$click.on('click', function() {
+		$(this).html('Peace Out!');
+		$(this).fadeOut(1500);
 		return false;
 	});
 
 	$submit.on('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$.each($(this).find('input'), function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
+	$(document).ready( function() {
+
+		var title = document.cookie.split("=");
+		
+		if(title[1] != undefined) {
+			$("#title").html(title[1]);
+		}
+
+
+		setTimeout(function() {
 			$timeout.fadeIn('slow');
 		}, 1000);
 	});
